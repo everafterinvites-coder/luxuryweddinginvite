011a42c78029dffa59c8dc6f532fa1dbbd48baa4
everafterinvites <contact.everafterinvites@gmail.com>
2026-06-07 05:01:49 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/luxuryweddinginvite/', // Updated base path for GitHub Pages
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
